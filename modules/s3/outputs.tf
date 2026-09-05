dfaa56a5784aa569c6545b44f8c9fc7334eb4169
youhualok <youhua1933@gmail.com>
2026-09-05 16:30:00 +0800

    Update S3 module outputs to reference instaed of data source

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,7 +1,11 @@
 output "bucket_name" {
-  value = data.aws_s3_bucket.uploads.id
+  value = aws_s3_bucket.uploads.id
 }
 
 output "bucket_id" {
-  value = data.aws_s3_bucket.uploads.id
+  value = aws_s3_bucket.uploads.id
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.uploads.arn
 }
